8d641085ef97e98bbf393d73f017c55e2fe509f6
AmyG01 <gammtonjeni@gmail.com>
2026-09-15 22:42:08 +0200

    customers and orders tables overwrite due to date columns updated datatypes

--- notebooks/silver.py
+++ notebooks/silver.py
@@ -43,12 +43,6 @@ orders_silver = orders_bronze \
 
 # COMMAND ----------
 
-customers_silver.write.mode("overwrite").saveAsTable("silver_customers")
-orders_silver.write.mode("overwrite").saveAsTable("silver_orders")
-products_silver.write.mode("overwrite").saveAsTable("silver_products")
-
-# COMMAND ----------
-
 from pyspark.sql.functions import to_date, col
 
 customers_silver = customers_silver.withColumn(
@@ -62,6 +56,21 @@ orders_silver = orders_silver.withColumn(
     "order_date",
     to_date(col("order_date"), "yyyy-MM-dd")
 )
+
+# COMMAND ----------
+
+customers_silver.write \
+    .mode("overwrite") \
+    .option("overwriteSchema", "true") \
+    .saveAsTable("silver_customers")
+    
+orders_silver.write \
+    .mode("overwrite") \
+    .option("overwriteSchema", "true") \
+    .saveAsTable("orders_silver")
+    
+
+products_silver.write.mode("overwrite").saveAsTable("silver_products")
 
 # COMMAND ----------
 
